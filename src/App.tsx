c1805c2ccbdca864d23e899f66580f3e7b8a2b8a
Vladimir Bystrov <vladimir.bystrov@beamtrail.com>
2025-02-03 16:57:24 +0400

    initial project template setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>React Sample</h1>
+
+      <p>Sample content, for simple example</p>
+    </>
+  );
+};
+
+export default App;
